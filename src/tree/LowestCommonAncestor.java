0ab44cf2a034f24757c181bb17e6eea3f2ab9668
chenguanjie <chenguanjie@black-unique.com>
2022-02-08 11:50:18 +0800

    最近公共è祖先

---
+++ src/tree/LowestCommonAncestor.java
@@ -0,0 +1,86 @@
+package tree;
+
+/**
+ @Description 二叉搜索树的最近公共祖先 简单
+ @Author chengj
+ @Date 2022/1/29
+ */
+
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。”
+//
+// 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5]
+//
+//
+//
+//
+//
+// 示例 1:
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+//输出: 6
+//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+//
+//
+// 示例 2:
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+//输出: 2
+//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+//
+//
+//
+// 说明:
+//
+//
+// 所有节点的值都是唯一的。
+// p、q 为不同节点且均存在于给定的二叉搜索树中。
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 747 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class LowestCommonAncestor {
+
+    /**
+     * 我们从根节点开始遍历；
+     *
+     * 如果当前节点的值大于 p 和 q 的值，说明 p 和 q 应该在当前节点的左子树，因此将当前节点移动到它的左子节点；
+     *
+     * 如果当前节点的值小于 p 和 q 的值，说明 p 和 q 应该在当前节点的右子树，因此将当前节点移动到它的右子节点；
+     *
+     * 如果当前节点的值不满足上述两条要求，那么说明当前节点就是「分岔点」。此时，p 和 q 要么在当前节点的不同的子树中，要么其中一个就是当前节点。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/solution/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-26/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ans = root;
+        while (true){
+            if (p.val < ans.val && q.val < ans.val){
+                ans = ans.left;
+            }else if (p.val > ans.val && q.val > ans.val){
+                ans = ans.right;
+            }else {
+                break;
+            }
+        }
+        return ans;
+    }
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int x) { val = x; }
+     }
+
+}
